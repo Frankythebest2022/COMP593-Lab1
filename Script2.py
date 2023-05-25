3aef0a81cec830d85a20ab448b458b7e956d1e49
Frankythebest2022 <fchoupo@flemingcollege.ca>
2023-05-24 21:44:08 -0400

    Modified both files in the Repo clone
    - Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- Script2.py
+++ Script2.py
@@ -1,3 +1,5 @@
 #printlines from Zen of Python
 print("Simple is better than complex.")
 print("Complex is better than complicated.")
+print("Flat is better than nested.")
+print("Sparse is better than dense.")
